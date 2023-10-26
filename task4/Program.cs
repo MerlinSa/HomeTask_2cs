d59acac26d5d3a8cc7bf2ca70ca7e3ac96a3269d
Saikala <s.borubaevaa@gmail.com>
2023-10-26 22:15:58 +0600

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает натуральное число N, 
+//а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите натур.число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(String.Join(",", Enumerable.Range(1, n)));
+Console.ReadLine();
