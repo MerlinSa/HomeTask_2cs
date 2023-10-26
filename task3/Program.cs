3d649fee3ae01d101fb5342670c57f8155901d4c
Saikala <s.borubaevaa@gmail.com>
2023-10-26 22:15:24 +0600

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа.
+
+Console.WriteLine("Введите двухзначное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x >= 10 && x <= 99)
+{
+    int a = x / 10;
+    int b = x % 10;
+    if (a > b)
+    {
+        Console.Write(a + " is max");
+    }
+    else{
+        Console.Write(b + " is max");
+    }
+}
+else
+{
+    Console.WriteLine("Введенное число вне отрезка");
+}
